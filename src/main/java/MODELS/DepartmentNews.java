5de833f5bddb661990b69b0c1e052a4038ae29bc
Joyce Muriithi <muriithi.joyce@eclectics.io>
2021-06-24 22:53:51 +0300

    Add consructor in class Department news

--- src/main/java/MODELS/DepartmentNews.java
+++ src/main/java/MODELS/DepartmentNews.java
@@ -1,4 +1,13 @@
 package MODELS;
 
+import java.sql.Timestamp;
+
 public class DepartmentNews extends News{
+    private int departmentId;
+
+
+    public DepartmentNews(int id, int userid, String type, String content, Timestamp postdate,int departmentId) {
+        super(id, userid, type, content, postdate);
+        this.departmentId=departmentId;
+    }
 }
