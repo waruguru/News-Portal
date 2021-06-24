235704f456f17a2dab2721242850a034417ffab7
Joyce Muriithi <muriithi.joyce@eclectics.io>
2021-06-24 22:57:06 +0300

    Add constructor in class user

--- src/main/java/MODELS/User.java
+++ src/main/java/MODELS/User.java
@@ -6,4 +6,12 @@ public class User {
     private String position;
     private String role;
     private int departmentId;
+
+    public User(int id, String name, String position, String role, int departmentId) {
+        this.id = id;
+        this.name = name;
+        this.position = position;
+        this.role = role;
+        this.departmentId = departmentId;
+    }
 }
