f64521c32ac3fbbf3acf9bead354ada4e7b69f72
Joyce Muriithi <muriithi.joyce@eclectics.io>
2021-06-24 21:29:33 +0300

    Add constructorr in department

--- src/main/java/MODELS/Department.java
+++ src/main/java/MODELS/Department.java
@@ -4,4 +4,10 @@ public class Department {
     private int id;
     private String name;
     private String description;
+
+    public Department(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
 }
