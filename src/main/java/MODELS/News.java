2e32941110703997c99d11af4bebc5964cb30fc4
Joyce Muriithi <muriithi.joyce@eclectics.io>
2021-06-24 22:49:35 +0300

    Add constructor in class News

--- src/main/java/MODELS/News.java
+++ src/main/java/MODELS/News.java
@@ -9,5 +9,12 @@ public class News {
     private String content;
     private Timestamp postdate;
 
+    public News(int id, int userid, String type, String content, Timestamp postdate) {
+        this.id = id;
+        this.userid = userid;
+        this.type = type;
+        this.content = content;
+        this.postdate = postdate;
+    }
 
 }
