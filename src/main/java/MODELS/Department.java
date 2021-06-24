bb7f093c62d4abecdfe298b8905a456d89b7fc35
Joyce Muriithi <muriithi.joyce@eclectics.io>
2021-06-24 21:31:20 +0300

    add methods with annotations override in depaertment

--- src/main/java/MODELS/Department.java
+++ src/main/java/MODELS/Department.java
@@ -1,5 +1,7 @@
 package MODELS;
 
+import java.util.Objects;
+
 public class Department {
     private int id;
     private String name;
@@ -34,5 +36,35 @@ public class Department {
     public void setDescription(String description) {
         this.description = description;
     }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return id == that.id &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(description, that.description);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description);
+    }
+
+    public static class DepartmentWithUserCount extends  Department {
+        private int count;
+
+        public DepartmentWithUserCount(int id, String name, String description,int count) {
+            super(id, name, description);
+            this.count=count;
+        }
+
+        public int getCount() {
+            return count;
+        }
+        public void setCount(int count) {
+            this.count = count;
+        }
+    }
+
 
 }
