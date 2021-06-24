1e354af71fd6f62175d033e28fa3592c9bee32ca
Joyce Muriithi <muriithi.joyce@eclectics.io>
2021-06-24 22:57:45 +0300

    Add the getters and setters methods in class user

--- src/main/java/MODELS/User.java
+++ src/main/java/MODELS/User.java
@@ -14,4 +14,45 @@ public class User {
         this.role = role;
         this.departmentId = departmentId;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
 }
