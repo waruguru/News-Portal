44564d1ea2b3fc7cd70dc61176fd13040f02ad0e
Joyce Muriithi <muriithi.joyce@eclectics.io>
2021-06-24 22:58:29 +0300

    Add the hashcode and the equals method in class user

--- src/main/java/MODELS/User.java
+++ src/main/java/MODELS/User.java
@@ -1,5 +1,7 @@
 package MODELS;
 
+import java.util.Objects;
+
 public class User {
     private int id;
     private String name;
@@ -55,4 +57,16 @@ public class User {
         this.departmentId = departmentId;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && departmentId == user.departmentId && Objects.equals(name, user.name) && Objects.equals(position, user.position) && Objects.equals(role, user.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, position, role, departmentId);
+    }
 }
